be8b58964c1cd8ebd2c433cf17b61fe6ff13c6ac
Shambhavi Kumar <shambhavikumar0214@gmail.com>
2025-12-08 22:01:39 +0530

    Create day3_Q2.c

---
+++ day3_Q2.c
@@ -0,0 +1,17 @@
+Write a program to swap two numbers using a third variable.
+
+  #include <stdio.h>
+
+int main() {
+    int a, b, temp;
+
+    scanf("%d %d", &a, &b);
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("After swapping: a = %d, b = %d", a, b);
+
+    return 0;
+}
