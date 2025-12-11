f130e84072cc68ca810a4395847bed2e83f0760d
Shambhavi Kumar <shambhavikumar0214@gmail.com>
2025-12-11 19:37:08 +0530

    Create day28_Q2.c

---
+++ day28_Q2.c
@@ -0,0 +1,10 @@
+//Read and print elements of a one-dimensional array.
+
+#include <stdio.h>
+int main() {
+    int n,i,a[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++) scanf("%d",&a[i]);
+    for(i=0;i<n;i++) printf("%d ", a[i]);
+    return 0;
+}
