a5b5f1c490758fd43e2763431decce23675c84ac
Shambhavi Kumar <shambhavikumar0214@gmail.com>
2025-12-11 19:58:31 +0530

    Create day39_Q2.c

---
+++ day39_Q2.c
@@ -0,0 +1,13 @@
+//Find the sum of main diagonal elements for a square matrix.
+
+#include <stdio.h>
+int main() {
+    int n,i,j,a[10][10],sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+        for(j=0;j<n;j++)
+            scanf("%d",&a[i][j]);
+    for(i=0;i<n;i++) sum+=a[i][i];
+    printf("%d",sum);
+    return 0;
+}
