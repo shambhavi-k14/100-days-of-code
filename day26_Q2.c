1825819a1759b87c59d99e1c6caf46bbbca8b0d7
Shambhavi Kumar <shambhavikumar0214@gmail.com>
2025-12-11 19:33:52 +0530

    Create day26_Q2.c

---
+++ day26_Q2.c
@@ -0,0 +1,35 @@
+//Write a program to print the following pattern:
+
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*
+
+
+#include <stdio.h>
+int main() {
+    int i;
+    for(i=1;i<=1;i++) printf("*\n");
+    printf("\n");
+    for(i=1;i<=2;i++) printf("*\n");
+    printf("\n");
+    for(i=1;i<=4;i++) printf("*\n");
+    printf("\n");
+    for(i=1;i<=3;i++) printf("*\n");
+    printf("\n");
+    for(i=1;i<=1;i++) printf("*\n");
+    return 0;
+}
