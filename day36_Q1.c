19b3d47a1db21b3894267f8a19c1030f4adfe273
Shambhavi Kumar <shambhavikumar0214@gmail.com>
2025-12-11 19:51:09 +0530

    Create day36_Q1.c

---
+++ day36_Q1.c
@@ -0,0 +1,16 @@
+//Read and print a matrix.
+
+#include <stdio.h>
+int main() {
+    int r,c,i,j,a[10][10];
+    scanf("%d%d",&r,&c);
+    for(i=0;i<r;i++)
+        for(j=0;j<c;j++)
+            scanf("%d",&a[i][j]);
+    for(i=0;i<r;i++){
+        for(j=0;j<c;j++)
+            printf("%d ",a[i][j]);
+        printf("\n");
+    }
+    return 0;
+}
