ff9658e5347e4606225a8d36b2ad8d7dea58b3ee
Shambhavi Kumar <shambhavikumar0214@gmail.com>
2025-12-11 19:48:32 +0530

    Create day34_Q2.c

---
+++ day34_Q2.c
@@ -0,0 +1,13 @@
+//Delete an element from an array.
+
+#include <stdio.h>
+int main() {
+    int n,i,pos,a[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++) scanf("%d",&a[i]);
+    scanf("%d",&pos);
+    for(i=pos;i<n-1;i++) a[i]=a[i+1];
+    n--;
+    for(i=0;i<n;i++) printf("%d ",a[i]);
+    return 0;
+}
